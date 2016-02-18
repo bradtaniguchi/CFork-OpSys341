d4bd4a58f2ef935efe0a9a90bef7cb9909f399cb
braddtaniguchi <bradtaniguchi@gmail.com>
2016-02-17 17:14:29 -0800

    Added very basic main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+/* 
+ * Bradley Taniguchi
+ *
+ * CFork-OpSys341
+ * Operating Systems 341
+ * Dr. Bin Tang
+ * 02/17/16
+ */
+
+#include <stdio.h>
+#include <unistd.h> 
+
+int main() {
+    return 0;
+}
