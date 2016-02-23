0cbee9bf26151fa452b5606589ff862c5cb60cda
bradtaniguchi <bradtaniguchi@gmail.com>
2016-02-22 23:09:24 -0800

    Updated cthread.c

--- cthread.c
+++ cthread.c
@@ -4,7 +4,9 @@
  * Part two of CSC341 Programming project 2
  * using pthreads
  */
-#include <stdio.h>
+#include <stdio.h> //For printline
+#include <pthread.h>
+#include <stdlib.h>
 #include "headers.h" //for original functions
 
 int main(int argc, char** argv)  {
@@ -14,7 +16,19 @@ int main(int argc, char** argv)  {
         return -1;
     }
     else {
-        pidTest();
+        argc = argc - 1; 
+        argv = argv + 1;
+        int i, len, test;
+        int myArray[argc];
+        for(i=0; i < argc; i++) {
+            myArray[i] = atoi(argv[i]);
+            len++; //new array length after checking
+        }
+        pthread_t minthread, maxthread, avgthread; //declare threads
+        test = pthread_create(&minthread, NULL, getMin(int len, int* myArray), NULL);
+
+        //pthread_t thread1;
+        // integer1 = pthread_create(&thread1, NULL, print_message_function, (void*) message1)
     }
     return 0;
 }
